698b991536aee5738da595f180f99b3f9b03de25
Harini3 <thaigaiharini1@gmail.com>
2023-02-21 14:33:27 +0530

    Second Commit

--- src/main/java/com/example/demo/StudentEntity/Student.java
+++ src/main/java/com/example/demo/StudentEntity/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.StudentEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,6 +10,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
+@Table
 public class Student {
 
 	@Id
